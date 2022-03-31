37e5d4c1f1e609de975d919af14e613e63d35462
Hunter6058 <39561057+Hunter6058@users.noreply.github.com>
2022-03-31 06:30:09 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
     include_once 'header.php';
 ?>
-    <title>Home</title>
-    <div class="index_body">
-        <section>
-            <h2> This Is An Great Front Page</h1>
-        </section>
+    <div class="page-content">
+      <section style="text-align:center; color:white; padding-top:248px;">
+          <h1>SEARCH CATALOG</h1>
+      </section>
+      <div class="search">
+          <div class="search-container">
+            <form action="/insertlink.php">
+              <input type="text" placeholder="Search..." name="search" style=text-align:center;>
+              <button type="submit">
+                <img src="https://cdn0.iconfinder.com/data/icons/very-basic-2-android-l-lollipop-icon-pack/24/search-512.png" width=20 height=20>
+              </button>
+            </form>
+          </div>
+        </div>
     </div>
 <?php
     include_once 'footer.php';
